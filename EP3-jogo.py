bbca26f8082ccd910e87465ea887ea9c47c5cb82
brunop2702 <brunop2702@hotmail.com>
2016-04-20 11:00:40 -0300

    verifica_ganhador testado

--- EP3-jogo.py
+++ EP3-jogo.py
@@ -1,9 +1,10 @@
 class jogo:
+    jogadas=[]
     matriz=[[1,2,3],[4,5,6],[7,8,9]] 
     def ___init___(self,matriz):
         self.matriz=matriz
         
-    def recebe_jogada(self,linha,coluna, jogadas):  
+    def recebe_jogada(self,linha,coluna):  
         if len(jogadas) % 2 == 0:
             self.matriz[linha][coluna]='X'
             jogadas.append(1)  
@@ -15,7 +16,7 @@ class jogo:
             
         
     def verifica_ganhador(self):
-        if self.jogadas>4: 
+        if len(jogadas)>4: 
             if self.matriz[0][0]=='X' and self.matriz [0][1]=='X' and self.matriz[0][2]=='X':
                 return 1
             if self.matriz[1][0]=='X' and self.matriz[1][1]=='X' and self.matriz[1][2]=='X':
@@ -48,12 +49,15 @@ class jogo:
                 return 2
             if self.matriz[0][2]=='O' and self.matriz[1][1]=='O' and self.matriz[2][0]=='O':
                 return 2
-        if self.jogadas==9:
+        if len(jogadas)==9:
             return 0
+        else:
+            return -1
             
-jogadas=[]
 jogo=jogo()
 
 for i in range(0,3):
     for j in range(0,3):
-        print(jogo.recebe_jogada(i,j,jogadas))
+        print(jogo.recebe_jogada(i,j)) 
+        print(jogo.verifica_ganhador())
+jogadas=[]
