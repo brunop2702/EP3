642c4f98d2a97de4308df7a432056f01b26d4d9b
brunop2702 <brunop2702@hotmail.com>
2016-04-25 18:52:39 -0300

    código arrumado

--- EP3-jogo.py
+++ EP3-jogo.py
@@ -1,22 +1,24 @@
-class jogo:
-    jogadas=[]
-    matriz=[[1,2,3],[4,5,6],[7,8,9]] 
-    def ___init___(self,matriz):
-        self.matriz=matriz
+class Jogo:
+     
+    def __init__(self):
+        self.jogadas=[]
+        self.matriz=[[1,2,3],[4,5,6],[7,8,9]]
+        #self.matriz=matriz
+        #self.jogadas=jogadas
         
     def recebe_jogada(self,linha,coluna):  
-        if len(jogadas) % 2 == 0:
+        if len(self.jogadas) % 2 == 0:
             self.matriz[linha][coluna]='X'
-            jogadas.append(1)  
+            self.jogadas.append(1)  
             return 'X' 
         else:
             self.matriz[linha][coluna]='O'
-            jogadas.append(1)  
+            self.jogadas.append(1)  
             return 'O' 
             
         
     def verifica_ganhador(self):
-        if len(jogadas)>4: 
+        if len(self.jogadas)>4: 
             if self.matriz[0][0]=='X' and self.matriz [0][1]=='X' and self.matriz[0][2]=='X':
                 return 1
             if self.matriz[1][0]=='X' and self.matriz[1][1]=='X' and self.matriz[1][2]=='X':
@@ -49,7 +51,7 @@ class jogo:
                 return 2
             if self.matriz[0][2]=='O' and self.matriz[1][1]=='O' and self.matriz[2][0]=='O':
                 return 2
-        if len(jogadas)==9:
+        if len(self.jogadas)==9:
             return 0
         else:
             return -1
@@ -71,7 +73,7 @@ class jogo:
             return ('Jogador O ganhou')
             
             
-jogo=jogo()
+jogo=Jogo()
 
 for i in range(0,3):
     for j in range(0,3):
@@ -79,4 +81,5 @@ for i in range(0,3):
         print(jogo.verifica_ganhador())
         print(jogo.matriz) 
         print(jogo.limpa_jogadas())
-jogadas=[] 
+print(jogo.jogadas)
+print(jogo.matriz)
